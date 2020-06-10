e59d186c477c65b594fe5cb33b0d50aaa5fc5344
ongiao <zqchenjunxing@163.com>
2020-06-11 03:51:06 +0800

    Add string funtions and test cases

--- string.go
+++ string.go
@@ -4,6 +4,7 @@ import (
 	"html"
 	"log"
 	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -62,12 +63,19 @@ func KebabCase(str string) string {
 
 	processedStr := strings.TrimSpace(reg.ReplaceAllString(str, " "))
 
+	allUpperCase := true
 	var upperCaseIndexes []int
-
 	for i, ch := range processedStr {
 		if ch >= 65 && ch <= 90 {
 			upperCaseIndexes = append(upperCaseIndexes, i)
+		} else if ch == 32 {
+		} else {
+			allUpperCase = false
 		}
+	}
+
+	if allUpperCase {
+		return strings.ReplaceAll(strings.ToLower(processedStr), " ", "-")
 	}
 
 	if len(upperCaseIndexes) > 0 {
@@ -91,4 +99,230 @@ func KebabCase(str string) string {
 	}
 
 	return res
+}
+
+func LowerCase(str string) string {
+	reg, err := regexp.Compile("[^a-zA-Z]+")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	processedStr := strings.TrimSpace(reg.ReplaceAllString(str, " "))
+
+	allUpperCase := true
+	var upperCaseIndexes []int
+	for i, ch := range processedStr {
+		if ch >= 65 && ch <= 90 {
+			upperCaseIndexes = append(upperCaseIndexes, i)
+		} else if ch == 32 {
+		} else {
+			allUpperCase = false
+		}
+	}
+
+	if allUpperCase {
+		return strings.ToLower(processedStr)
+	}
+
+	if len(upperCaseIndexes) > 0 {
+		for _, index := range upperCaseIndexes {
+			if index != 0 && processedStr[index-1] != ' ' {
+				processedStr = processedStr[:index] + " " + processedStr[index:]
+			}
+		}
+	}
+
+	return strings.ToLower(processedStr)
+}
+
+func LowerFirst(str string) string {
+	return strings.ToLower(str[:1]) + str[1:]
+}
+
+func Pad(str string, length int, chars string) string {
+	if length <= len(str) {
+		return str
+	}
+
+	repeatCharsHead := strings.Repeat(chars, length)
+	repeatCharsTail := strings.Repeat(chars, length)
+	startIndex := (length - len(str)) / 2
+
+	return repeatCharsHead[:startIndex] + str + repeatCharsTail[:length - startIndex - len(str)]
+}
+
+func PadEnd(str string, length int, chars string) string {
+	if length <= len(str) {
+		return str
+	}
+
+	repeatChars := strings.Repeat(chars, length)
+
+	return str + repeatChars[:length - len(str)]
+}
+
+func PadStart(str string, length int, chars string) string {
+	if length <= len(str) {
+		return str
+	}
+
+	repeatChars := strings.Repeat(chars, length)
+
+	return repeatChars[:length - len(str)] + str
+}
+
+func ParseInt(str string, radix int) int {
+	bitSize := 32 << (^uint(0) >> 63)
+	i, err := strconv.ParseInt(str, radix, bitSize)
+
+	if err != nil {
+		log.Fatal("Parse int error!")
+	}
+
+	return int(i)
+}
+
+func Repeat(str string, n int) string {
+	return strings.Repeat(str, n)
+}
+
+func Replace(str string, old, replacement string, n int) string {
+	return strings.Replace(str, old, replacement, n)
+}
+
+func SnakeCase(str string) string {
+	reg, err := regexp.Compile("[^a-zA-Z]+")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	processedStr := strings.TrimSpace(reg.ReplaceAllString(str, " "))
+
+	allUpperCase := true
+	var upperCaseIndexes []int
+	for i, ch := range processedStr {
+		if ch >= 65 && ch <= 90 {
+			upperCaseIndexes = append(upperCaseIndexes, i)
+		} else if ch == 32 {
+		} else {
+			allUpperCase = false
+		}
+	}
+
+	if allUpperCase {
+		return strings.ReplaceAll(strings.ToLower(processedStr), " ", "_")
+	}
+
+	if len(upperCaseIndexes) > 0 {
+		for _, index := range upperCaseIndexes {
+			if index != 0 && processedStr[index-1] != ' ' {
+				processedStr = processedStr[:index] + " " + processedStr[index:]
+			}
+		}
+	}
+
+	stringSlice := strings.Split(strings.ToLower(processedStr), " ")
+
+	var res string
+
+	for i, n := range stringSlice {
+		if i == len(stringSlice) - 1 {
+			res += n
+		} else {
+			res += n + "_"
+		}
+	}
+
+	return res
+}
+
+func Split(str, separator string) []string {
+	return strings.Split(str, separator)
+}
+
+func SplitLimit(str, separator string, limit int) []string {
+	res := strings.Split(str, separator)
+
+	return res[:limit]
+}
+
+func StartCase(str string) string {
+	reg, err := regexp.Compile("[^a-zA-Z]+")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	processedStr := strings.TrimSpace(reg.ReplaceAllString(str, " "))
+
+	allUpperCase := true
+	var upperCaseIndexes []int
+	for i, ch := range processedStr {
+		if ch >= 65 && ch <= 90 {
+			upperCaseIndexes = append(upperCaseIndexes, i)
+		} else if ch == 32 {
+		} else {
+			allUpperCase = false
+		}
+	}
+
+	if allUpperCase {
+		return processedStr
+	}
+
+	if len(upperCaseIndexes) > 0 {
+		for _, index := range upperCaseIndexes {
+			if index != 0 && processedStr[index-1] != ' ' {
+				processedStr = processedStr[:index] + " " + processedStr[index:]
+			}
+		}
+	}
+
+	stringSlice := strings.Split(strings.ToLower(processedStr), " ")
+
+	var res string
+
+	for i, n := range stringSlice {
+		if i == len(stringSlice) - 1 {
+			res += strings.Title(n)
+		} else {
+			res += strings.Title(n) + " "
+		}
+	}
+
+	return res
+}
+
+func StartsWith(str, target string, position int) bool {
+	if position >= len(str) {
+		return false
+	}
+
+	if position < 0 {
+		position = 0
+	}
+
+	return strings.Index(str[position:], target) == 0
+}
+
+func ToLower(str string) string {
+	return strings.ToLower(str)
+}
+
+func ToUpper(str string) string {
+	return strings.ToUpper(str)
+}
+
+func Trim(str, chars string) string {
+	return strings.Trim(str, chars)
+}
+
+func TrimEnd(str, chars string) string {
+	return strings.TrimRight(str, chars)
+}
+
+func TrimStart(str, chars string) string {
+	return strings.TrimLeft(str, chars)
 }
--- string_test.go
+++ string_test.go
@@ -100,36 +100,398 @@ func TestKebabCase(t *testing.T) {
 		got = KebabCase(str3)
 		want = "get-foo-bar-tar"
 
+		str4 := "__FOO_BAR__"
+		got = KebabCase(str4)
+		want = "foo-bar"
+
 		assertEqual(t, got, want, "KebabCase failed")
 	})
 }
 
-//func TestLowerCase(t *testing.T) {
-//	t.Run("Test lower case", func(t *testing.T) {
-//		str1 := "--Foo-Bar--"
-//
-//		got := LowerCase(str1)
-//		want := "foo bar"
-//
-//		assertEqual(t, got, want, "KebabCase failed")
-//
-//		str2 := "fooBar"
-//
-//		got = LowerCase(str2)
-//		want = "foo bar"
-//
-//		assertEqual(t, got, want, "KebabCase failed")
-//
-//		str3 := "__FOO_BAR__"
-//
-//		got = LowerCase(str3)
-//		want = "foo bar"
-//
-//		fmt.Println(got, want)
-//
-//		assertEqual(t, got, want, "KebabCase failed")
-//	})
-//}
+func TestLowerCase(t *testing.T) {
+	t.Run("Test lower case", func(t *testing.T) {
+		str1 := "--Foo-Bar--"
+
+		got := LowerCase(str1)
+		want := "foo bar"
+
+		assertEqual(t, got, want, "LowerCase failed")
+
+		str2 := "fooBar"
+
+		got = LowerCase(str2)
+		want = "foo bar"
+
+		assertEqual(t, got, want, "LowerCase failed")
+
+		str3 := "__FOO_BAR__"
+
+		got = LowerCase(str3)
+		want = "foo bar"
+
+		assertEqual(t, got, want, "LowerCase failed")
+	})
+}
+
+func TestLowerFirst(t *testing.T) {
+	t.Run("Test lower first", func(t *testing.T) {
+		str1 := "Fred"
+
+		got := LowerFirst(str1)
+		want := "fred"
+
+		assertEqual(t, got, want, "LowerFirst failed")
+
+		str2 := "FRED"
+
+		got = LowerFirst(str2)
+		want = "fRED"
+
+		assertEqual(t, got, want, "LowerFirst failed")
+	})
+}
+
+func TestPad(t *testing.T) {
+	t.Run("Test pad", func(t *testing.T) {
+		str1 := "abc"
+
+		got := Pad(str1, 8, " ")
+		want := "  abc   "
+
+		assertEqual(t, got, want, "Pad failed")
+
+		str2 := "abc"
+
+		got = Pad(str2, 8, "_-")
+		want = "_-abc_-_"
+
+		assertEqual(t, got, want, "Pad failed")
+
+		str3 := "abc"
+
+		got = Pad(str3, 3, "")
+		want = "abc"
+
+		assertEqual(t, got, want, "Pad failed")
+	})
+}
+
+func TestPadEnd(t *testing.T) {
+	t.Run("Test pad end", func(t *testing.T) {
+		str1 := "abc"
+
+		got := PadEnd(str1, 6, " ")
+		want := "abc   "
+
+		assertEqual(t, got, want, "Pad end failed")
+
+		str2 := "abc"
+
+		got = PadEnd(str2, 6, "_-")
+		want = "abc_-_"
+
+		assertEqual(t, got, want, "Pad end failed")
+
+		str3 := "abc"
+
+		got = PadEnd(str3, 3, "")
+		want = "abc"
+
+		assertEqual(t, got, want, "Pad end failed")
+	})
+}
+
+func TestPadStart(t *testing.T) {
+	t.Run("Test pad start", func(t *testing.T) {
+		str1 := "abc"
+
+		got := PadStart(str1, 6, " ")
+		want := "   abc"
+
+		assertEqual(t, got, want, "Pad start failed")
+
+		str2 := "abc"
+
+		got = PadStart(str2, 6, "_-")
+		want = "_-_abc"
+
+		assertEqual(t, got, want, "Pad start failed")
+
+		str3 := "abc"
+
+		got = PadStart(str3, 3, "")
+		want = "abc"
+
+		assertEqual(t, got, want, "Pad start failed")
+	})
+}
+
+func TestParseInt(t *testing.T) {
+	t.Run("Test parse int", func(t *testing.T) {
+		str1 := "08"
+
+		got := ParseInt(str1, 10)
+		want := 8
+
+		assertEqual(t, got, want, "ParseInt failed")
+
+		str2 := "15"
+
+		got = ParseInt(str2, 16)
+		want = 21
+
+		assertEqual(t, got, want, "ParseInt failed")
+	})
+}
+
+func TestRepeat(t *testing.T) {
+	t.Run("Test repeat", func(t *testing.T) {
+		str1 := "*"
+
+		got := Repeat(str1, 3)
+		want := "***"
+
+		assertEqual(t, got, want, "Pad start failed")
+
+		str2 := "abc"
+
+		got = Repeat(str2, 2)
+		want = "abcabc"
+
+		assertEqual(t, got, want, "Pad start failed")
+
+		str3 := "abc"
+
+		got = Repeat(str3, 0)
+		want = ""
+
+		assertEqual(t, got, want, "Pad start failed")
+	})
+}
+
+func TestReplace(t *testing.T) {
+	t.Run("Test replace", func(t *testing.T) {
+		str1 := "Hi Fred"
+
+		got := Replace(str1, "Hi", "Hello", 2)
+		want := "Hello Fred"
+
+		assertEqual(t, got, want, "Replace failed")
+
+		str2 := "Hi Fred, Hi World"
+
+		got = Replace(str2, "Hi", "Hello", 2)
+		want = "Hello Fred, Hello World"
+
+		assertEqual(t, got, want, "Replace failed")
+	})
+}
+
+func TestSnakeCase(t *testing.T) {
+	t.Run("Test lower case", func(t *testing.T) {
+		str1 := "Foo Bar"
+
+		got := SnakeCase(str1)
+		want := "foo_bar"
+
+		assertEqual(t, got, want, "SnakeCase failed")
+
+		str2 := "fooBar"
+
+		got = SnakeCase(str2)
+		want = "foo_bar"
+
+		assertEqual(t, got, want, "SnakeCase failed")
+
+		str3 := "--FOO-BAR--"
+
+		got = SnakeCase(str3)
+		want = "foo_bar"
+
+
+		assertEqual(t, got, want, "SnakeCase failed")
+	})
+}
+
+func TestSplit(t *testing.T) {
+	t.Run("Test replace", func(t *testing.T) {
+		str1 := "a-b-c"
+
+		got := Split(str1, "-")
+		want := []string{"a", "b", "c"}
+
+		assertDeepEqual(t, got, want, "Split failed")
+	})
+}
+
+func TestSplitLimit(t *testing.T) {
+	t.Run("Test splitLimit", func(t *testing.T) {
+		str1 := "a-b-c"
+
+		got := SplitLimit(str1, "-", 2)
+		want := []string{"a", "b"}
+
+		assertDeepEqual(t, got, want, "SplitLimit failed")
+	})
+}
+
+func TestStartCase(t *testing.T) {
+	t.Run("Test start case", func(t *testing.T) {
+		str1 := "--foo-bar--"
+
+		got := StartCase(str1)
+		want := "Foo Bar"
+
+		assertEqual(t, got, want, "StartCase failed")
+
+		str2 := "fooBar"
+
+		got = StartCase(str2)
+		want = "Foo Bar"
+
+		assertEqual(t, got, want, "StartCase failed")
+
+		str3 := "__FOO_BAR__"
+
+		got = StartCase(str3)
+		want = "FOO BAR"
+
+
+		assertEqual(t, got, want, "StartCase failed")
+	})
+}
+
+func TestStartsWith(t *testing.T) {
+	t.Run("Test starts with", func(t *testing.T) {
+		str1 := "abc"
+
+		got := StartsWith(str1, "a", 0)
+		want := true
+
+		assertEqual(t, got, want, "StartsWith failed")
+
+		str2 := "abc"
+
+		got = StartsWith(str2, "b", 0)
+		want = false
+
+		assertEqual(t, got, want, "StartsWith failed")
+
+		str3 := "abc"
+
+		got = StartsWith(str3, "b", 1)
+		want = true
+
+
+		assertEqual(t, got, want, "StartsWith failed")
+	})
+}
+
+func TestToLower(t *testing.T) {
+	t.Run("Test to lower", func(t *testing.T) {
+		str1 := "--Foo-Bar--"
+
+		got := ToLower(str1)
+		want := "--foo-bar--"
+
+		assertEqual(t, got, want, "ToLower failed")
+
+		str2 := "fooBar"
+
+		got = ToLower(str2)
+		want = "foobar"
+
+		assertEqual(t, got, want, "ToLower failed")
+
+		str3 := "__FOO_BAR__"
+
+		got = ToLower(str3)
+		want = "__foo_bar__"
+
+
+		assertEqual(t, got, want, "ToLower failed")
+	})
+}
+
+func TestToUpper(t *testing.T) {
+	t.Run("Test to upper", func(t *testing.T) {
+		str1 := "--Foo-Bar--"
+
+		got := ToUpper(str1)
+		want := "--FOO-BAR--"
+
+		assertEqual(t, got, want, "ToUpper failed")
+
+		str2 := "fooBar"
+
+		got = ToUpper(str2)
+		want = "FOOBAR"
+
+		assertEqual(t, got, want, "ToUpper failed")
+
+		str3 := "__FOO_BAR__"
+
+		got = ToUpper(str3)
+		want = "__FOO_BAR__"
+
+
+		assertEqual(t, got, want, "ToUpper failed")
+	})
+}
+
+func TestTrim(t *testing.T) {
+	t.Run("Test trim", func(t *testing.T) {
+		str1 := "  abc  "
+
+		got := Trim(str1, " ")
+		want := "abc"
+
+		assertEqual(t, got, want, "Trim failed")
+
+		str2 := "-_-abc-_-"
+
+		got = Trim(str2, "_-")
+		want = "abc"
+
+		assertEqual(t, got, want, "Trim failed")
+	})
+}
+
+func TestTrimEnd(t *testing.T) {
+	t.Run("Test trim end", func(t *testing.T) {
+		str1 := "  abc  "
+
+		got := TrimEnd(str1, " ")
+		want := "  abc"
+
+		assertEqual(t, got, want, "TrimEnd failed")
+
+		str2 := "-_-abc-_-"
+
+		got = TrimEnd(str2, "_-")
+		want = "-_-abc"
+
+		assertEqual(t, got, want, "TrimEnd failed")
+	})
+}
+
+func TestTrimStart(t *testing.T) {
+	t.Run("Test trim start", func(t *testing.T) {
+		str1 := "  abc  "
+
+		got := TrimStart(str1, " ")
+		want := "abc  "
+
+		assertEqual(t, got, want, "TrimStart failed")
+
+		str2 := "-_-abc-_-"
+
+		got = TrimStart(str2, "_-")
+		want = "abc-_-"
+
+		assertEqual(t, got, want, "TrimStart failed")
+	})
+}
 
 func assertEqual(t *testing.T, got, want interface{}, errMsg string) {
 	t.Helper()
